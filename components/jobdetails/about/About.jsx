6db95049afa46db9cc7f4e6145fbb7a7b8555ba3
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2023-09-25 16:12:29 +0400

    complete tutorial

---
+++ components/jobdetails/about/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import styles from "./about.style";
+
+const About = ({ info }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headText}>About the job:</Text>
+      <Text style={styles.contentBox}>
+        <Text style={styles.contextText}>{info}</Text>
+      </Text>
+    </View>
+  );
+};
+
+export default About;
